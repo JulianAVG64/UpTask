981094466338699452f62b949121952c6a366ff8
JulianAVG64 <julvargasgu@unal.edu.co>
2025-01-21 14:48:31 -0500

    Add task button, Modal with JavaScript, no SCC

---
+++ src/js/tareas.js
@@ -0,0 +1,33 @@
+(function() {
+    // Botón para mostrar el Modal de Agregar tarea
+    const nuevaTareaBtn = document.querySelector('#agregar-tarea');
+    nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+
+    function mostrarFormulario() {
+        const modal = document.createElement('DIV');
+        modal.classList.add('modal');
+        modal.innerHTML = `
+            <form class="formulario nueva-tarea">
+                <legend>Añade una nueva tarea</legend>
+                <div class="campo">
+                    <label>Tarea</label>
+                    <input
+                        type="text"
+                        name="tarea"
+                        placeholder="Añadir Tarea al Proyecto Actual"
+                        id="tarea"
+                    />
+                </div>
+                <div class="opciones">
+                    <input type="submit" class="submit-nueva-tarea" value="Añadir Tarea"/>
+                    <button type="button" class="cerrar-modal">Cancelar</button>
+                </div>
+
+            </form>
+        `;
+
+        document.querySelector('body').appendChild(modal);
+    }
+
+})(); //IIEF, Función que se manda llamar inmediatamente. Proteger las variables para que no se mezclen con los otros archivos
+
