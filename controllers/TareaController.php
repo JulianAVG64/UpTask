730c5c3eea5325c9ab8ee99a1447ccb3f65b746c
JulianAVG64 <julvargasgu@unal.edu.co>
2025-01-22 15:34:41 -0500

    Obtaining Project, validate and Insert Task in DB

--- controllers/TareaController.php
+++ controllers/TareaController.php
@@ -2,6 +2,9 @@
 
 namespace Controllers;
 
+use Model\Proyecto;
+use Model\Tarea;
+
 class TareaController {
     public static function index() {
 
@@ -10,11 +13,31 @@ class TareaController {
     public static function crear() {
         
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
-            
+
+            session_start();
+
+            $proyectoId = $_POST['proyectoId'];
+
+            $proyecto = Proyecto::where('url', $proyectoId);
+
+            if(!$proyecto || $proyecto->propietarioId !== $_SESSION['id']) {
+                $respuesta = [
+                    'tipo' => 'error',
+                    'mensaje' => 'Hubo un error al agregar la tarea'
+                ];
+                echo json_encode($respuesta);
+                return;
+            }
+
+            // Todo bien, instanciar y crear la tarea
+            $tarea = new Tarea($_POST);
+            $tarea->proyectoId = $proyecto->id;
+            $resultado = $tarea->guardar();
             $respuesta = [
-                'proyectoId' => $_POST['proyectoId']
+                'tipo' => 'exito',
+                'id' => $resultado['id'],
+                'mensaje' => 'Tarea Creada Correctamente'
             ];
-            
             echo json_encode($respuesta);
         }
     }
